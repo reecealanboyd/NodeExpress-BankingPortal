6e7cd76bd245a99af2191ae8296935bdec31e6fa
Reece Boyd <rboyd36@gatech.edu>
2019-06-05 22:50:09 -0500

    module 5 done

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const { accounts, users, writeJSON } = require('./data');
+const accountRoutes = require('./routes/accounts');
+const servicesRoutes = require('./routes/services');
 
 const app = express();
 
@@ -10,47 +12,15 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({extended: true}));
+app.use('/account', accountRoutes);
+app.use('/services', servicesRoutes);
 
 app.get('/', (req, res) => {
   res.render('index', {title: 'Account Summary', accounts });
 });
 
-app.get('/savings', (req, res) => {
-  res.render('account', { account: accounts.savings });
-});
-
-app.get('/checking', (req, res) => {
-  res.render('account', { account: accounts.checking });
-});
-
-app.get('/credit', (req, res) => {
-  res.render('account', { account: accounts.credit });
-});
-
 app.get('/profile', (req, res) => {
   res.render('profile', { user: users[0] });
-});
-
-app.get('/transfer', (req, res) => {
-  res.render('transfer');
-});
-
-app.post('/transfer', (req, res) => {
-  accounts[req.body.from].balance = parseInt(accounts[req.body.from].balance) - parseInt(req.body.amount);
-  accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount);
-  writeJSON();
-  res.render('transfer', { message: "Transfer Completed" });
-});
-
-app.get('/payment', (req, res) => {
-  res.render('payment', { account: accounts.credit });
-});
-
-app.post('/payment', (req, res) => {
-  accounts.credit.balance = parseInt(accounts.credit.balance) - parseInt(req.body.amount);
-  accounts.credit.available = parseInt(accounts.credit.available) + parseInt(req.body.amount);
-  writeJSON();
-  res.render('payment', { message: "Payment Successful", account: accounts.credit });
 });
 
 app.listen(3000, () => {
